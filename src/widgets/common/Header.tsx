e13eb2c3ace47d92f81130af2431be9cf296a1ab
d5ng <dong5ffice@gmail.com>
2025-04-27 21:41:25 +0900

    ✨ Feat: add Header Component

---
+++ src/widgets/common/Header.tsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+import Button from "@shared/ui/Button"
+
+export default function Header() {
+  return (
+    <header className="px-4 py-3 border-b border-gray-200">
+      <div className="flex justify-between">
+        <h1>
+          <Link to="/" className="flex gap-2">
+            <img src="/logo.svg" alt="롤링 페이퍼" />
+            <span className="text-gray-600 font-bold text-xl">Rolling</span>
+          </Link>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Button asChild variant="outlined" className="px-4">
+                <Link to="/create">롤링 페이퍼 만들기</Link>
+              </Button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
