9f6394eb893d6ae02d38123e475d8d6bf2a40719
d5ng <dong5ffice@gmail.com>
2025-04-29 17:15:22 +0900

    ✨ Feat: add Header Component

--- src/widgets/common/Header.tsx
+++ src/widgets/common/Header.tsx
@@ -4,17 +4,17 @@ import Button from "@shared/ui/Button"
 export default function Header() {
   return (
     <header className="px-4 py-3 border-b border-gray-200">
-      <div className="flex justify-between">
+      <div className="flex justify-between xl:max-w-[1200px] xl:mx-auto">
         <h1>
           <Link to="/" className="flex gap-2">
-            <img src="/logo.svg" alt="롤링 페이퍼" />
+            <img src="/logo.svg" alt="메인 페이지 바로가기" />
             <span className="text-gray-600 font-bold text-xl">Rolling</span>
           </Link>
         </h1>
         <nav>
           <ul>
             <li>
-              <Button asChild variant="outlined" className="px-4">
+              <Button asChild variant="outlined" className="px-4 xl:w-[157px]">
                 <Link to="/create">롤링 페이퍼 만들기</Link>
               </Button>
             </li>
