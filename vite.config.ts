de8d1fb3734cc557ba4f7f399c25650856082874
d5ng <dong5ffice@gmail.com>
2025-04-30 11:38:25 +0900

    자잘한 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from "vitest/config"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom"
-  }
+  plugins: [react(), tsconfigPaths()]
 })
